fa21d1f41b219c2884da38f13913a0d2ff38ebe4
Daniel Waldman <dwaldman@berkeley.edu>
2019-10-18 19:46:35 -0700

    adding 61b final project

--- proj3/byow/Core/Hallway.java
+++
@@ -1,9 +0,0 @@
-package byow.Core;
-
-public class Hallway extends IndoorArea {
-    //make sure length or width one has to be = to one
-    public Hallway(Point startingPoint, int length, int width, TileWorld tw) {
-        super(startingPoint, length, width, tw);
-        build(startingPoint);
-    }
-}
